94c67c21460297eb07eeaafe569233c236ebee4a
Michael <dannte@gmail.com>
2021-12-07 08:25:47 +0200

    started to work on session

---
+++ src/Sessions/session.js
@@ -0,0 +1,29 @@
+import {DateTime} from 'luxon'
+import {Timestamp} from 'firebase/firestore'
+
+export default class Session {
+    constructor (date, name=null) {
+        this.date = date
+        this.name = name
+    }
+
+    toString() {
+        const name = this.name || ''
+        const formatted_date = this.date.toLocaleString(DateTime.DATE_SHORT)
+        return `${name}: session to play at ${formatted_date}`
+    }
+}
+
+export const SessionConvertor = {
+    toFirestore: session => {
+        return {
+            date: Timestamp.fromDate(session.date.toJSDate())
+        }
+    },
+    fromFireStore: (snapshot, options) => {
+        const data = snapshot.data(options)
+        return new Session(
+            DateTime.fromJSDate(data.date.toDate())
+        )
+    }
+}
