e070952466f51f9718da4444171b36d5e95470c1
Michael <dannte@gmail.com>
2022-02-14 17:49:25 +0200

    updated the locationsVIew

--- src/Sessions/viewers/LocationsView.jsx
+++ src/Sessions/viewers/LocationsView.jsx
@@ -2,19 +2,24 @@ import React from "react";
 import PropTypes from 'prop-types'
 
 import {Typography, Collapse} from 'antd'
+import _ from 'lodash'
 
 import { SECTION_COLORS } from "../../common/consts";
 import { DESCRIPTION_STYLE } from "../SessionViewer";
+import "./LocationsView.css"
 
 const {Title} = Typography
 
-// TODO: play with the style of the collapse to make it more compact
+const LENGTH_OF_DEFAULT_OPEN = 120
+
 export default function LocationsView({locations}) {
-    const all_keys = locations.map(l => l.name)
+    const open_keys = 
+        locations.filter(l =>  _.size(l.description) < LENGTH_OF_DEFAULT_OPEN && !_.isEmpty(l.description))
+        .map(l => l.name)
     
-    return <div style={{ ...DESCRIPTION_STYLE, backgroundColor: SECTION_COLORS.locations }}>
+    return <div style={{ ...DESCRIPTION_STYLE, backgroundColor: SECTION_COLORS.locations }} className="locations-view">
         <Title level={4}>מקומות</Title>
-        <Collapse ghost defaultActiveKey={all_keys}>
+        <Collapse ghost defaultActiveKey={open_keys}>
             {locations.map(({name, description}) => {
                 return <Collapse.Panel key={name} header={<b>{name}</b>} >
                     {description}
